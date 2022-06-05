326beafd28e608ee37dd95ba8f6afd39950473b9
Tung Nguyen <tung.nv2510@gmail.com>
2022-06-05 23:23:50 +0700

    Setup store ok

--- src/aaRedux/app/store.js
+++ src/aaRedux/app/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 
 // import the reducer
+import userReducer from './userSlice';
 
 
 
 const rootReducer = {
   // add the reducer here
+  user: userReducer,
 };
 
 const store = configureStore({
